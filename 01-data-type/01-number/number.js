b58f94a7b16ac2a067dc8308cd71612c00b0481d
danieldificil <daniel.araujo@ideabox-it.com>
2022-09-12 02:15:01 -0300

    type: number

---
+++ 01-data-type/01-number/number.js
@@ -0,0 +1,30 @@
+//tipo Number
+
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.4;
+const pontoFlutuanteSemZero = .7;
+const novaOperacaoMatematica = numeroPontoFlutuante + pontoFlutuanteSemZero;
+
+console.log(novaOperacaoMatematica);
+
+// NaN (Not a Number)
+
+const exemplo = "exemplo";
+const numZero = 0;
+
+console.log(exemplo * meuNumero);
+console.log(numZero / numZero);
+
+// infinity
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
